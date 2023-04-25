5d1b3c8bba2a86b049dc1e1068d9e9392919588b
armens <simonyanarmen9601@gmail.com>
2023-04-25 23:06:53 +0400

    add ci pipeline

---
+++ user-service/src/main/java/com/wallet/userservice/security/CurrentUser.java
@@ -0,0 +1,21 @@
+//package com.wallet.userservice.security;
+//
+//import com.energizeglobal.auth.service.core.entity.User;
+//import org.springframework.security.core.authority.AuthorityUtils;
+//
+//public class CurrentUser extends org.springframework.security.core.userdetails.User {
+//
+//  private final User user;
+//
+//  public CurrentUser(User user) {
+//    super(
+//        user.getEmail(),
+//        user.getPassword(),
+//        AuthorityUtils.createAuthorityList(user.getRole().name()));
+//    this.user = user;
+//  }
+//
+//  public User getUser() {
+//    return user;
+//  }
+//}
